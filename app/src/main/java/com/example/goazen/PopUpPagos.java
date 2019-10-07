f98ba71d64fd96793029a41772a8a4c10a15330e
Jessica Rubio <rubiovargas.jessica@gmail.com>
2019-10-07 13:07:07 +0200

    Version 2.3
    Iniciamos la creación del pop up de pagos

---
+++ app/src/main/java/com/example/goazen/PopUpPagos.java
@@ -0,0 +1,14 @@
+package com.example.goazen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopUpPagos extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_pagos);
+    }
+}
