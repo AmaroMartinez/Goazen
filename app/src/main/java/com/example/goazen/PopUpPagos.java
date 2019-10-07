497464d749d7548f42c40c41cdedcea99448900f
Jessica Rubio <rubiovargas.jessica@gmail.com>
2019-10-07 13:29:51 +0200

    Version 2.3
    Iniciamos la creación del pop up de pagos

--- app/src/main/java/com/example/goazen/PopUpPagos.java
+++ app/src/main/java/com/example/goazen/PopUpPagos.java
@@ -3,12 +3,21 @@ package com.example.goazen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
-public class PopUpPagos extends Activity {
+public class PopUpPagos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up_pagos);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width));
     }
 }
