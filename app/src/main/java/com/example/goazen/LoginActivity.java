3f9f31954e791e02201884d983749b3784689632
Jon Ander De Castro <45819953+SsrJon@users.noreply.github.com>
2019-10-15 12:36:49 +0200

    S2.IV.II Login verificación

--- app/src/main/java/com/example/goazen/LoginActivity.java
+++ app/src/main/java/com/example/goazen/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.goazen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,10 +10,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.goazen.Administrador.MainActivityAdmin;
 import com.example.goazen.Cliente.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import static android.content.ContentValues.TAG;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +29,9 @@ public class LoginActivity extends AppCompatActivity {
     Spinner desplegableusuarios;
     private Button btnLoginEntrar;
     private EditText editTextLoginUsuario, editTextLoginContraseña;
+    private static FirebaseFirestore db;
+    private static String DNI;
+    private static String Contrasena;
 
     //Metodo onCreate
     @Override
@@ -38,8 +50,33 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
+                db = FirebaseFirestore.getInstance();
+                DocumentReference docRef = db.collection("Usuarios").document(editTextLoginUsuario.getText().toString());
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
 
-                if(editTextLoginUsuario.getText().toString().equals("cliente")&&editTextLoginContraseña.getText().toString().equals("cliente")){
+                                DNI = document.getString("DNI");
+                                Contrasena = document.getString("Contrasena");
+
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+
+
+
+
+                if(editTextLoginUsuario.getText().toString().equals(DNI)&&editTextLoginContraseña.getText().toString().equals(Contrasena)){
 
                     Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(myIntent);
