46059cf2711fc4ed54edb8a550f17a9c80f88d90
AmaroMartinez <45819622+AmaroMartinez@users.noreply.github.com>
2019-10-16 14:12:18 +0200

    Menu trabajador funciona mal

--- app/src/main/java/com/example/goazen/Trabajador/MainActivity_Trabajador.java
+++ app/src/main/java/com/example/goazen/Trabajador/MainActivity_Trabajador.java
@@ -1,26 +1,18 @@
 package com.example.goazen.Trabajador;
 
 import android.os.Bundle;
+import android.view.Menu;
 
-import com.example.goazen.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.goazen.R;
 import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
 
 public class MainActivity_Trabajador extends AppCompatActivity {
 
@@ -30,25 +22,18 @@ public class MainActivity_Trabajador extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__trabajador);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar_trabajador);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_trabajador);
+        NavigationView navigationView = findViewById(R.id.nav_view_trabajador);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_perfil_trabajador, R.id.nav_calendario_trabajador, R.id.nav_nominas_trabajador)
+                R.id.nav_perfil, R.id.nav_calendario_trabajador, R.id.nav_nominas_trabajador)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_trabajador);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
@@ -62,7 +47,7 @@ public class MainActivity_Trabajador extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_trabajador);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
