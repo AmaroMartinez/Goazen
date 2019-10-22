f6c4fa36fbae159e7bd8a69b1ee99e946d4e74e6
Jessica Rubio <rubiovargas.jessica@gmail.com>
2019-10-22 13:22:18 +0200

    S2.XI.I --> Habilitar y deshabilitar servicios         --> Comenzamos a programar cada uno de los switch

--- app/src/main/java/com/example/goazen/Administrador/ui/servicios/AdminServiciosFragment.java
+++ app/src/main/java/com/example/goazen/Administrador/ui/servicios/AdminServiciosFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -18,18 +19,36 @@ public class AdminServiciosFragment extends Fragment {
 
     private AdminServiciosViewModel adminServiciosViewModel;
 
+    /*Declaramos los Switch que tenemos en el XML para poder programarlos*/
+    private Switch Sw_servicio_admin_limpieza_general;
+    private Switch Sw_servicio_admin_limpieza_cristales;
+    private Switch Sw_servicio_admin_lavanderia;
+    private Switch Sw_servicio_admin_paseo_mascotas;
+    private Switch Sw_servicio_admin_regado_plantas;
+    private Switch Sw_servicio_admin_plancha;
+    private Switch Sw_servicio_admin_cocina;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         adminServiciosViewModel =
                 ViewModelProviders.of(this).get(AdminServiciosViewModel.class);
         View root = inflater.inflate(R.layout.fragment_admin_servicios, container, false);
-        final TextView textView = root.findViewById(R.id.text_send_admin);
-        adminServiciosViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        //Inicializamos todas las opciones de servicios existentes
+        Sw_servicio_admin_limpieza_general = root.findViewById(R.id.sw_servicio_admin_limpieza_general);
+        Sw_servicio_admin_limpieza_cristales = root.findViewById(R.id.sw_servicio_admin_limpieza_cristales);
+        Sw_servicio_admin_lavanderia = root.findViewById(R.id.sw_servicio_admin_lavanderia);
+        Sw_servicio_admin_paseo_mascotas = root.findViewById(R.id.sw_servicio_admin_paseo_mascotas);
+        Sw_servicio_admin_regado_plantas = root.findViewById(R.id.sw_servicio_admin_regado_plantas);
+        Sw_servicio_admin_plancha = root.findViewById(R.id.sw_servicio_admin_plancha);
+        Sw_servicio_admin_cocina = root.findViewById(R.id.sw_servicio_admin_cocina);
+
+        /*Programamos la funcionalidad de cada uno de ellos teniendo en cuenta que cada vez que el
+        * estado cambie se actualizara en la base de datos.
+        * */
+
+       
+
         return root;
     }
 }
