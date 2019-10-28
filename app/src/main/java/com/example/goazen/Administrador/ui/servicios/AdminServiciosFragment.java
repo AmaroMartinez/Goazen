3f320502aebc696581f3e2ac8a6f3366674b08d1
Jessica Rubio <rubiovargas.jessica@gmail.com>
2019-10-28 08:44:23 +0100

    S2.XI.I guardado actualizado para que funcione

--- app/src/main/java/com/example/goazen/Administrador/ui/servicios/AdminServiciosFragment.java
+++ app/src/main/java/com/example/goazen/Administrador/ui/servicios/AdminServiciosFragment.java
@@ -3,35 +3,29 @@ package com.example.goazen.Administrador.ui.servicios;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.goazen.R;
 import com.example.goazen.Servicios;
-import com.example.goazen.Trabajador.ui.calendario.RecyclerViewCalTrabajador;
 import com.example.goazen.Values;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.Timestamp;
+
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Date;
 
-import static android.content.ContentValues.TAG;
 
 public class AdminServiciosFragment extends Fragment {
 
@@ -67,25 +61,19 @@ public class AdminServiciosFragment extends Fragment {
         Sw_servicio_admin_plancha = root.findViewById(R.id.sw_servicio_admin_plancha);
         Sw_servicio_admin_cocina = root.findViewById(R.id.sw_servicio_admin_cocina);
 
-        /*Inicializamos los botones*/
+        /*Inicializamos los botones
+         * Para ello primero nos descargamos de la base de datos los datos y despues
+         * configuramos su estado dependiendo de lo que diga la descarga.*/
         ConfigurarPantalla();
-        for(int pos = 0; pos<ListaServicios.size(); pos++){
-            switch(ListaServicios.get(pos).getNombre_servicio()){
-                case getString(R.string.st_limpieza_general):
-                    Sw_servicio_admin_limpieza_general.setChecked(ListaServicios.get(pos).isEnable());
-                    break;
-                default:
 
-            }
-
-
-        }
+        Log.e(Values.tag_log, "longitud lista: " + ListaServicios.size() + ". Tras configurar");
         /*Programamos la funcionalidad de cada uno de ellos teniendo en cuenta que cada vez que el
-        * estado cambie se actualizara en la base de datos.*/
+         * estado cambie se actualizara en la base de datos.*/
 
         Sw_servicio_admin_limpieza_general.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
                 actualizarEstado(Sw_servicio_admin_limpieza_general.getText().toString(),isChecked);
             }
         });
@@ -98,10 +86,23 @@ public class AdminServiciosFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
         //Indicamos la colección a la que vamos a acceder
         CollectionReference Servicios = db.collection("Servicios");
+       /* Map<String, Object> datos = new HashMap<>();
+        double precio = 0;
+        for (int pos =0;pos<ListaServicios.size();pos++){
+            if(ListaServicios.get(pos).getNombre_servicio().equals(servicio)){
+             precio = ListaServicios.get(pos).getPrecio();
+            }
+        }
+        datos.put("Precio",precio);
+        datos.put("Enable", estado);
+        Servicios.document(servicio);*/
+
 
     }
 
+
     public void ConfigurarPantalla (){
+        Log.e(Values.tag_log, "entramos en configurarPantalla");
         ListaServicios = new ArrayList<Servicios>();
         db = FirebaseFirestore.getInstance();
         db.collection("Servicios")
@@ -113,10 +114,44 @@ public class AdminServiciosFragment extends Fragment {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 /*Cogemos los datos de cada uno de los servicios y los introducimo
                                 en el array*/
-                                ListaServicios.add(new Servicios(document.getId().toString(),document.getBoolean("Enable"), document.getDouble("precio")));
+                                Log.d(Values.tag_log,document.getId() +" --> "+ document.getBoolean("Enable")+" --> "+document.getDouble("Precio"));
+                                ListaServicios.add(new Servicios(document.getId(),document.getBoolean("Enable"), document.getDouble("Precio")));
+                                Log.e(Values.tag_log, String.valueOf(ListaServicios.size()));
                             }
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        if(task.isComplete()){
+                            for(int pos = 0; pos<ListaServicios.size(); pos++){
+                                Log.e(Values.tag_log,"Entramos en el bucle");
+                                switch(ListaServicios.get(pos).getNombre_servicio()) {
+                                    case Values.SERV_GENERAL:
+                                        Log.d(Values.tag_log, "Hemos entrado en el case limpieza general");
+                                        Sw_servicio_admin_limpieza_general.setChecked(ListaServicios.get(pos).getEnable());
+                                        break;
+                                    case Values.SERV_CRISTALES:
+                                        Sw_servicio_admin_limpieza_cristales.setChecked(ListaServicios.get(pos).getEnable());
+                                        break;
+                                    case Values.SERV_COCINA:
+                                        Sw_servicio_admin_cocina.setChecked(ListaServicios.get(pos).getEnable());
+                                        break;
+                                    case Values.SERV_LAVANDERIA:
+                                        Sw_servicio_admin_lavanderia.setChecked(ListaServicios.get(pos).getEnable());
+                                        break;
+                                    case Values.SERV_PASEO:
+                                        Sw_servicio_admin_paseo_mascotas.setChecked(ListaServicios.get(pos).getEnable());
+                                        break;
+                                    case Values.SERV_PLANCHA:
+                                        Sw_servicio_admin_plancha.setChecked(ListaServicios.get(pos).getEnable());
+                                        break;
+                                    case Values.SERV_REGADO:
+                                        Sw_servicio_admin_regado_plantas.setChecked(ListaServicios.get(pos).getEnable());
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+
+                        }else{
+                            Log.d(Values.tag_log, "Error getting documents: ", task.getException());
                         }
                     }
                 });
